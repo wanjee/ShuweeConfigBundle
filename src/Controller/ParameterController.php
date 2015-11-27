36558d92466a1ac43bd7132e396ae17a14f7bd77
wanjee <wanjee@users.noreply.github.com>
2015-11-27 17:10:32 +0100

    Unused use statement should be avoided
    Follow-up SensioLabsInsights analysis

--- src/Controller/ParameterController.php
+++ src/Controller/ParameterController.php
@@ -4,7 +4,6 @@ namespace Wanjee\Shuwee\ConfigBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
